1c431846eb91411dbd1b677a90454843abbd2db5
jonathan <bitbucket@jmartz.de>
2018-12-30 14:18:59 +0100

    move auth data into object

--- provider/AuthServiceProvider.php
+++ provider/AuthServiceProvider.php
@@ -1,66 +1,65 @@
 <?php
 
-	namespace App\Providers;
+namespace App\Providers;
 
-	use App\User;
-	use Illuminate\Support\ServiceProvider;
-	use Illuminate\Auth\GenericUser;
-	use Illuminate\Support\Facades\DB;
-	use Illuminate\Support\Facades\Validator;
+use App\User;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\GenericUser;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
+class AuthServiceProvider extends ServiceProvider{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register(){
+        //
+    }
 
+    public $request;
 
-	class AuthServiceProvider extends ServiceProvider{
-		/**
-		 * Register any application services.
-		 *
-		 * @return void
-		 */
-		public function register(){
-			//
-		}
+    /**
+     * Boot the authentication services for the application.
+     *
+     * @return void
+     */
+    public function boot(){
+        $this->app['auth']->viaRequest('api', function($request){
+            $validator = Validator::make($request->all(), [
+                'auth.username' => 'required|string',
+                'auth.userid' => 'required|integer',
+                'auth.token' => 'required|string|size:512'
+            ])->validate();
 
-		public $request;
+            $users = DB::connection('mysql.write')
+                ->table('users')
+                ->where('username','=',$request->input('auth.username'))
+                ->where('username_hash','=',sha1($request->input('auth.username')));
 
-		/**
-		 * Boot the authentication services for the application.
-		 *
-		 * @return void
-		 */
-		public function boot(){
-			$this->app['auth']->viaRequest('api', function($request){
-				$validator = Validator::make($request->all(), [
-					'username' => 'required',
-					'token' => 'required|size:512'
-				])->validate();
+            $count = $users->count();
 
-				$users = DB::connection('mysql.write')
-						   ->table('users')
-						   ->where('username','=',$request->input('username'))
-						   ->where('username_hash','=',sha1($request->input('username')));
+            $user = $users->first();
 
-				$count = $users->count();
+            if($count === 1){
+                $tokens = DB::connection('mysql.write')
+                    ->table('auth_tokens')
+                    ->where('UID','=',$user->id)
+                    ->where('token','=',$request->input('auth.token'));
+                if($tokens->count() === 1){
+                    return new User((array) $user);
+                }
+                else{
+                    $this->addMessage('error','Token doesnt exists.');
+                    return $this->getResponse();
+                }
+            }
+            else{
+                $this->addMessage('error','User doesnt exists.');
+                return $this->getResponse();
+            }
 
-				$user = $users->first();
-
-				if($count === 1){
-					$tokens = DB::connection('mysql.write')
-								->table('auth_tokens')
-								->where('UID','=',$user->id)
-								->where('token','=',$request->input('token'));
-					if($tokens->count() === 1){
-						return new User((array) $user);
-					}
-					else{
-						$this->addMessage('error','Token doesnt exists.');
-						return $this->getResponse();
-					}
-				}
-				else{
-					$this->addMessage('error','User doesnt exists.');
-					return $this->getResponse();
-				}
-
-			});
-		}
-	}
+        });
+    }
+}
