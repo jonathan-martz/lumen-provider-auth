66f994ab2295380899545e8d70787a1f022ce445
jonathan <bitbucket@jmartz.de>
2019-09-13 21:51:37 +0200

    use default mysql connection

--- provider/AuthServiceProvider.php
+++ provider/AuthServiceProvider.php
@@ -35,8 +35,7 @@ class AuthServiceProvider extends ServiceProvider{
                 'auth.token' => 'required|string|size:512'
             ])->validate();
 
-            $users = DB::connection('mysql.write')
-                ->table('users')
+            $users = DB::table('users')
                 ->where('username','=',$request->input('auth.username'))
                 ->where('username_hash','=',sha1($request->input('auth.username')));
 
@@ -45,15 +44,21 @@ class AuthServiceProvider extends ServiceProvider{
             $user = $users->first();
 
             if($count === 1){
-                $tokens = DB::connection('mysql.read')
-                    ->table('auth_tokens')
+                $tokens = DB::table('auth_tokens')
                     ->where('UID','=',$user->id)
                     ->where('token','=',$request->input('auth.token'));
                 if($tokens->count() === 1){
                     return new User((array) $user);
                 }
+                else{
+                    $this->addMessage('error','Token doesnt exists.');
+                    return $this->getResponse();
+                }
             }
-            return null;
+            else{
+                $this->addMessage('error','User doesnt exists.');
+                return $this->getResponse();
+            }
 
         });
     }
